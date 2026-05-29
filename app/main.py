b8dcab4fa10940c5bc295619aaa485b5a284938d
Harrison Rengifo <harryson02@outlook.com>
2026-05-28 19:31:41 -0500

    fix: CORS abierto para produccion

--- app/main.py
+++ app/main.py
@@ -19,11 +19,8 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://gimnasio-frontend-tau.vercel.app"
-    ],
-    allow_credentials=True,
+    allow_origins=["*"],
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
